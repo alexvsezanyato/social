35638c659131eefcbed37aea44c397fd7e9dcc90
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2025-11-07 19:34:40 +0700

    Convert SQL keywords to uppercase. Delete some unused functions

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Services\User;
-use App\Services\App;
-
 class AuthController
 {
     public function login()
@@ -14,7 +11,7 @@ class AuthController
 
         $pdo = connect();
 
-        $statement = $pdo->prepare('select * from "user" where "login"=?');
+        $statement = $pdo->prepare('SELECT * FROM "user" WHERE "login"=?');
         $statement->execute([$login]);
         $user = $statement->fetch();
 
@@ -22,6 +19,7 @@ class AuthController
             echo '1';
             return;
         }
+
         $salt = $user['salt'];
         $hash = hash('sha512', $password . $salt);
 
@@ -29,6 +27,7 @@ class AuthController
             echo '1';
             return;
         }
+
         session_start();
         $_SESSION['id'] = $user['id'];
         $_SESSION['hash'] = hash('md5', $_SERVER['HTTP_USER_AGENT'] ?? 'ua');
@@ -37,7 +36,7 @@ class AuthController
         $cookie = $id . '-' . hash('sha256', $id . $random); 
 
         if (!isset($user['random'])) {
-            $statement = $pdo->prepare('update "user" set "random"=? where "id"=?');
+            $statement = $pdo->prepare('UPDATE "user" SET "random"=? WHERE "id"=?');
             $r = $statement->execute([$random, $id]);
 
             if (!$r) {
@@ -96,7 +95,7 @@ class AuthController
         $salt = base64_encode(random_bytes(32*0.66));
         $hash = hash('sha512', $password . $salt);
         $pdo = connect();
-        $statement = $pdo->prepare('select COUNT(*) AS "count" from "user" where "login"=?');
+        $statement = $pdo->prepare('SELECT COUNT(*) AS "count" FROM "user" WHERE "login"=?');
         $statement->execute([$login]);
         $count = $statement->fetch()['count'];
 
@@ -104,47 +103,9 @@ class AuthController
             echo '5';
             return;
         }
-        $query = 'insert into "user"("login", "age", "hash", "salt") values (?, ?, ?, ?)';
+
+        $query = 'INSERT INTO "user"("login", "age", "hash", "salt") VALUES (?, ?, ?, ?)';
         $pdo->prepare($query)->execute([$login, $age, $hash, $salt]);
         echo '0';
-    }
-
-    public function redirect(User $user)
-    {
-        if ($user->in()) { 
-            header('Loction: /');
-            exit;
-        }
-    }
-
-    public function session()
-    {
-        session_start();
-        echo 'cid: ' . $_SESSION['cid'];
-
-        // $cvcount = $_COOKIE['vcount'] ?? 0;
-        // setcookie('vcount', $cvcount, strtotime('+30 days'));
-
-        if (!array_key_exists('cid', $_SESSION)) {
-            $_SESSION['cid'] = 1;
-        }
-
-        $pdo = connect();
-
-        if ($pdo) {
-            // .
-            echo 'Success';
-        } else {
-            echo 'Fail';
-            die();
-        }
-
-        $statement = $pdo->query('select value from tdb where id=' . ($_SESSION['sid'] ?? null));
-        if ($statement) $row = $statement->fetch();
-        else $row = [];
-        $pdo->query('update tdb set value=' . ++$row['value'] . ' where id=' . ($_SESSION['sid'] ?? null));
-        echo ++$row['value'] ?? '';
-        phpinfo();
-        session_write_close();
     }
 }
