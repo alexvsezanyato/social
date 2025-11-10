6770c2c8effc7068090c848d4fe3452b6557f0f7
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2025-11-10 15:00:05 +0700

    refactor

---
+++ constants.php
@@ -0,0 +1,15 @@
+<?php
+
+define('RESOURCE_DIR', BASE_DIR.'/resources');
+define('VIEW_DIR', RESOURCE_DIR.'/views');
+define('CONFIG_DIR', BASE_DIR.'/config');
+define('STORAGE_DIR', BASE_DIR.'/storage');
+define('CACHE_DIR', STORAGE_DIR.'/cache');
+define('BOOTSTRAP_DIR', BASE_DIR.'/bootstrap');
+define('VENDOR_DIR', BASE_DIR.'/vendor');
+define('APP_DIR', BASE_DIR.'/app');
+define('ENTITY_DIR', APP_DIR.'/Entities');
+define('PUBLIC_DIR', BASE_DIR.'/public');
+define('UPLOAD_DIR', PUBLIC_DIR.'/uploads');
+
+define('DEBUG_MODE', true);
